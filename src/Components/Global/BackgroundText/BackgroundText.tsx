2a6fdedf78abdf54bb3fd870a4b7d8b967699088
Josef Hobler <pepous.hoblik@gmail.com>
2022-06-20 07:17:46 +0200

    Added background animation

--- src/Components/Global/BackgroundText/BackgroundText.tsx
+++ src/Components/Global/BackgroundText/BackgroundText.tsx
@@ -8,7 +8,7 @@ const BackgroundText: FC<Props> = ({ text }) => {
   return (
     <div
       style={{ fontSize: `16vw`, color: "rgb(36,40,44)" }}
-      className="bg-font position-absolute h-100 w-100 bg-font d-flex justify-content-center align-items-center animation-bg"
+      className="bg-font position-absolute h-100 w-100 bg-font d-flex justify-content-center align-items-center animation-fadeIn duration-5"
     >
       {text}
     </div>
